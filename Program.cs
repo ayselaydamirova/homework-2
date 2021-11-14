6f1622b1d4f20399fef526c3b5d5c252d93f56ee
Aysel <92165561+ayselaydamirova@users.noreply.github.com>
2021-11-14 19:07:36 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Homework_12._11._2001
+{
+    public class JsonData
+    {
+        public int Number { get; set; }
+        public string Text { get; set; }
+    }
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var JsonData = new JsonData
+            {
+                Number = 11,
+                Text = "Hello"
+            };
+
+            string JsonString = JsonSerializer.Serialize(JsonData);
+            Console.WriteLine(JsonString);
+        }
+
+    }
+}
+
+  
