0787292454318c736ced5327ba3be92e550c1375
aysel <aydamirovaaysel7@gmail.com>
2021-11-14 14:58:16 +0400

    Добавьте файлы проекта.

---
+++ homework-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> list = new List<string>();
+                string[] filesLocal = Directory.GetFiles(Directory.GetCurrentDirectory(), "", SearchOption.TopDirectoryOnly);
+                foreach (string file in filesLocal)
+                {
+                    list.Add(file);
+                }
+                String[] str = list.ToArray();
+                Console.WriteLine("{" + string.Join(",", list) + "}");
+                     
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Process finished with error --> {0}", e.ToString());
+            }
+        }
+    }
+}
